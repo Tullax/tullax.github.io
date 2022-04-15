62a465c8a185053e793a5603d9ba954d7d61ba2b
Attachment Studios <72270541+Attachment-Studios@users.noreply.github.com>
2022-04-15 18:37:14 +0530

    Tullax - Website Update
    Dynamic Website

---
+++ script.js
@@ -0,0 +1,11 @@
+function projects() {
+	let output = document.getElementById("nfts-list");
+
+	fetch("https://api.github.com/orgs/Tullax/repos").then(req => req.json()).then(data => {
+		for (let nft of data) {
+			if (nft.topics.includes("nft")) {
+				output.innerHTML += `<a href="${nft.homepage}"><button><img src="${nft.homepage}/Logo.png" /><br>${nft.name.replaceAll("-", " ")}</button></a>`;
+			}
+		}
+	});
+}
